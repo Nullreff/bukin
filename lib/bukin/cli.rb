9b8998063788bb16e08a9631c0fe9438d5d2076a
Ryan Mendivil <rsmendivil@gmail.com>
2013-03-10 19:03:10 -0800

    Add server command

--- lib/bukin/cli.rb
+++ lib/bukin/cli.rb
@@ -1,14 +1,53 @@
 require 'thor'
 require 'open-uri'
 require 'fileutils'
+require 'json'
 
-BASE = "http://api.bukget.org/3"
+# BukGet api
+# Docs: http://bukget.org/pages/docs/API3.html
+BUKGET_API = "http://api.bukget.org/3"
+
+# Bukkit download api
+# Docs: http://dl.bukkit.org/about/
+BUKKIT_API = "http://dl.bukkit.org/api/1.0/downloads" 
+
+# Base url used for bukkit downloads
+BUKKIT_BASE = "http://dl.bukkit.org"
+
+# Path to install plugins to
 PLUGINS_PATH = "plugins"
+
+SERVER_PATH = "."
 
 class Bukin::CLI < Thor
 
     def initialize(*)
         super
+    end
+
+    desc 'server NAME', "Download and install Minecraft server software"
+    option :version, :type => :string,
+                     :default => 'latest-rb',
+                     :aliases => '-v',
+                     :desc => "The version of the server to install"
+    def server(name)
+        name == 'craftbukkit' || abort("Currently only craftbukkit is supported as a sever")
+        version = options[:version]
+        view_url = "#{BUKKIT_API}/projects/#{name}/view/#{version}/"
+
+        begin
+            shell.say "Retriving the latest information about #{name}..."
+            view = JSON.parse(open(view_url).read)
+            url = BUKKIT_BASE + view['file']['url']
+            server_version = view['version']
+            display_version = pretty_version(version)
+
+            shell.say "Downloading #{display_version} of #{name}..."
+            file_name = download_to(url, SERVER_PATH)
+            shell.say "Saved to #{file_name}"
+        rescue OpenURI::HTTPError => ex
+            abort "Error: #{ex}"
+        end
     end
 
     desc 'install PLUGIN', "Download and install a plugin from bukkit dev"
@@ -22,21 +61,16 @@ class Bukin::CLI < Thor
                      :desc => "The server type this plugin works with"
     def install(plugin)
         version = options[:version]
+        display_version = pretty_version(version)
         server = options[:server]
-        url = "#{BASE}/plugins/#{server}/#{plugin}/#{version}/download"
+        url = "#{BUKGET_API}/plugins/#{server}/#{plugin}/#{version}/download"
 
         begin
-            open(url) do |download|
-                file_name = download.meta['content-disposition']
-                                    .match(/filename=(\"?)(.+)\1/)[2]
-
-                FileUtils.mkdir_p(PLUGINS_PATH)
-                open("#{PLUGINS_PATH}/#{file_name}", "wb") do |file|
-                    file.print download.read
-                end
-            end
+            shell.say "Downloading #{display_version} of #{plugin}..."
+            file_name = download_to(url, PLUGINS_PATH, true)
+            shell.say "Saved to #{file_name}"
         rescue OpenURI::HTTPError => ex
-            abort "Error downloading version #{version} of #{plugin}\n#{ex}"
+            abort "Error: #{ex}"
         end
     end
 
@@ -44,5 +78,33 @@ class Bukin::CLI < Thor
         shell.say "Bukin is a plugin and server package manager for Minecraft."
         shell.say
         super
+    end
+
+private
+
+    def download_to(url, path, content_disposition = false)
+        open(url) do |download|
+            file_name = if content_disposition
+                            download.meta['content-disposition']
+                                    .match(/filename=(\"?)(.+)\1/)[2]
+                        else
+                            File.basename(url)
+                        end
+
+            FileUtils.mkdir_p(path)
+            open("#{path}/#{file_name}", "wb") do |file|
+                file.print download.read
+            end
+
+            file_name
+        end
+    end
+
+    def pretty_version(version)
+        if version == 'latest'
+            "the latest version"
+        else
+           "version #{version}" 
+        end
     end
 end
