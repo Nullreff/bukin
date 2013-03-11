30b19b5739d13e183e4f740711952c30e2e0b79f
Ryan Mendivil <rsmendivil@gmail.com>
2013-03-11 00:21:22 -0800

    Add uninstall command

--- lib/bukin/cli.rb
+++ lib/bukin/cli.rb
@@ -2,6 +2,7 @@ require 'thor'
 require 'open-uri'
 require 'fileutils'
 require 'json'
+require 'bukin/lockfile'
 
 # BukGet api
 # Docs: http://bukget.org/pages/docs/API3.html
@@ -9,7 +10,7 @@ BUKGET_API = "http://api.bukget.org/3"
 
 # Bukkit download api
 # Docs: http://dl.bukkit.org/about/
-BUKKIT_API = "http://dl.bukkit.org/api/1.0/downloads" 
+BUKKIT_API = "http://dl.bukkit.org/api/1.0/downloads"
 
 # Base url used for bukkit downloads
 BUKKIT_BASE = "http://dl.bukkit.org"
@@ -17,11 +18,13 @@ BUKKIT_BASE = "http://dl.bukkit.org"
 # Path to install plugins to
 PLUGINS_PATH = "plugins"
 
+# Path to install the server in
 SERVER_PATH = "."
 
 class Bukin::CLI < Thor
 
     def initialize(*)
+        @lockfile = Bukin::Lockfile.new
         super
     end
 
@@ -33,20 +36,20 @@ class Bukin::CLI < Thor
     def server(name)
         name == 'craftbukkit' || abort("Currently only craftbukkit is supported as a sever")
         version = options[:version]
-        view_url = "#{BUKKIT_API}/projects/#{name}/view/#{version}/"
+        info_url = "#{BUKKIT_API}/projects/#{name}/view/#{version}/"
 
         begin
             shell.say "Retriving the latest information about #{name}..."
-            view = JSON.parse(open(view_url).read)
-            url = BUKKIT_BASE + view['file']['url']
-            server_version = view['version']
-            display_version = pretty_version(version)
+            info = JSON.parse(open(info_url).read)
+            url = BUKKIT_BASE + info['file']['url']
+            server_version = info['version']
 
-            shell.say "Downloading #{display_version} of #{name}..."
+            shell.say "Downloading version #{server_version} of #{name}..."
             file_name = download_to(url, SERVER_PATH)
+            @lockfile.set_server(name, server_version, file_name)
             shell.say "Saved to #{file_name}"
         rescue OpenURI::HTTPError => ex
-            abort "Error: #{ex}"
+            abort("Error: #{ex}")
         end
     end
 
@@ -59,19 +62,42 @@ class Bukin::CLI < Thor
                      :default => 'bukkit',
                      :aliases => '-s',
                      :desc => "The server type this plugin works with"
-    def install(plugin)
+    def install(name)
+        if @lockfile.plugins.has_key?(name)
+            abort("The plugin #{name} is already installed")
+        end
+
         version = options[:version]
         display_version = pretty_version(version)
         server = options[:server]
-        url = "#{BUKGET_API}/plugins/#{server}/#{plugin}/#{version}/download"
+        info_url = "#{BUKGET_API}/plugins/#{server}/#{name}/#{version}"
 
         begin
-            shell.say "Downloading #{display_version} of #{plugin}..."
-            file_name = download_to(url, PLUGINS_PATH, true)
+            shell.say "Retriving the latest information about #{name}..."
+            info = JSON.parse(open(info_url).read)
+            url = info['versions'][0]['download']
+            plugin_version = info['versions'][0]['version']
+
+            shell.say "Downloading version #{plugin_version} of #{name}..."
+            file_name = download_to(url, PLUGINS_PATH)
+            @lockfile.add_plugin(name, plugin_version, file_name)
             shell.say "Saved to #{file_name}"
         rescue OpenURI::HTTPError => ex
-            abort "Error: #{ex}"
+            abort("Error: #{ex}")
         end
+    end
+
+    desc 'uninstall PLUGIN', "Uninstalls a plugin"
+    def uninstall(name)
+        if not @lockfile.plugins.has_key?(name)
+            abort("The plugin #{name} is not currently installed")
+        end
+
+        version = @lockfile.plugins[name]['version']
+        files = @lockfile.plugins[name]['files']
+        files.each {|file| FileUtils.rm_f("#{PLUGINS_PATH}/#{file}")}
+        @lockfile.remove_plugin(name)
+        shell.say "Uninstalled version #{version} of #{name}"
     end
 
     def help(*)
@@ -115,7 +141,7 @@ private
         when /^build-(.*)$/
             "build \##{$1}"
         else
-           "version #{version}" 
+           "version #{version}"
         end
     end
 end
---
+++ lib/bukin/lockfile.rb
@@ -0,0 +1,61 @@
+require 'yaml'
+require 'fileutils'
+
+INSTALL_FILE = "Bukfile"
+LOCK_FILE = "#{INSTALL_FILE}.lock"
+
+class Bukin::Lockfile
+
+    def initialize(*)
+        if File.exist?(LOCK_FILE)
+            @lockfile = YAML::load_file(LOCK_FILE)
+        else
+            @lockfile = {
+                'server' => {},
+                'plugins' => {}
+            }
+        end
+    end
+
+    def set_server(name, version, file)
+        self.server = {
+            'name' => name,
+            'version' => version,
+            'file' => file
+        }
+    end
+
+    def add_plugin(name, version, *files)
+        self.plugins[name] = {
+            'version' => version,
+            'files' => files
+        }
+    end
+
+    def remove_plugin(name)
+        plugins.delete(name)
+        save
+    end
+
+    def plugins
+        @lockfile['plugins']
+    end
+
+    def plugins=(value)
+        @lockfile['plugins'] = value
+        save
+    end
+
+    def server
+        @lockfile['server']
+    end
+
+    def server=(value)
+        @lockfile['server'] = value
+        save
+    end
+
+    def save
+        File.open(LOCK_FILE, "w") {|file| file.write @lockfile.to_yaml}
+    end
+end
