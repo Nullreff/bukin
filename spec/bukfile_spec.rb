8d8d06e77803236f2bb87db9c7613e501153b8d3
Ryan Mendivil <rsmendivil@gmail.com>
2013-07-18 16:54:26 -0800

    Add more bukfile tests

--- spec/bukfile_spec.rb
+++ spec/bukfile_spec.rb
@@ -2,43 +2,71 @@ require 'spec_helper'
 require 'bukin'
 
 describe Bukin::Bukfile do
-  it 'adds a server' do
-    bukfile = Bukin::Bukfile.new do
-      server 'craftbukkit'
+  [:server, :plugin].each do |type|
+
+    it "adds a #{type} by method" do
+      bukfile = Bukin::Bukfile.new{}
+      bukfile.resources.size.should == 0
+
+      bukfile.send(type, '')
+      bukfile.resources.size.should == 1
     end
 
-    server = bukfile.resources.first
-    server[:name].should == 'craftbukkit'
-    server[:path].should == '.'
+    it "adds a #{type} by constructor" do
+      bukfile = Bukin::Bukfile.new do
+        send(type, '')
+      end
+
+      bukfile.resources.size.should == 1
+    end
+
+    it "adds a #{type} with the correct name" do
+      bukfile = Bukin::Bukfile.new do
+        send(type, 'resource_name')
+      end
+
+      bukfile.resources.first[:name].should == 'resource_name'
+    end
+
+    it "adds a #{type} with the correct path" do
+      bukfile = Bukin::Bukfile.new do
+        send(type, 'resource_name')
+      end
+
+      resource = bukfile.resources.first
+      case type
+      when :server
+        resource[:path].should == '.'
+      when :plugin
+        resource[:path].should == 'plugins'
+      end
+    end
+
+    it "adds a #{type} with the correct version" do
+      bukfile = Bukin::Bukfile.new do
+        send(type, '', '1.0.0')
+      end
+
+      bukfile.resources.first[:version].should == '1.0.0'
+    end
   end
 
-  it 'adds a plugin' do
+  it 'adds both plugins and servers' do
     bukfile = Bukin::Bukfile.new do
+      server 'craftbukkit'
       plugin 'worldedit'
     end
 
-    plugin = bukfile.resources.first
-    plugin[:name].should == 'worldedit'
-    plugin[:path].should == 'plugins'
+    bukfile.resources.size.should == 2
+    bukfile.resources.find{|resource| resource[:name] == 'craftbukkit'}.should_not be_nil
+    bukfile.resources.find{|resource| resource[:name] == 'worldedit'}.should_not be_nil
   end
 
-  it 'adds a server with a version' do
+  it 'will not add non-existant resources' do
     bukfile = Bukin::Bukfile.new do
-      server 'craftbukkit', '1.0.0'
     end
 
-    server = bukfile.resources.first
-    server[:name].should == 'craftbukkit'
-    server[:version].should == '1.0.0'
-  end
-
-  it 'adds a plugin with a version' do
-    bukfile = Bukin::Bukfile.new do
-      plugin 'worldedit', '1.0.0'
-    end
-
-    plugin = bukfile.resources.first
-    plugin[:name].should == 'worldedit'
-    plugin[:version].should == '1.0.0'
+    bukfile.resources.size.should == 0
+    bukfile.resources.find{|resource| resource[:name] == 'missing_plugin'}.should be_nil
   end
 end
