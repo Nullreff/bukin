e4db4dd96a64b93f2df4434b6d6218607eb2dbd9
Ryan Mendivil <rsmendivil@gmail.com>
2013-07-20 21:35:15 -0800

    Remove 'utils' class

--- lib/bukin/installer.rb
+++ lib/bukin/installer.rb
@@ -14,11 +14,11 @@ class Bukin::Installer
     dl_data, dl_name = download_file(data[:download])
 
     if File.extname(dl_name) == '.zip'
-      match = get_match(data[:extract])
+      match = self.get_match(data[:extract])
       file_names = extract_files(dl_data, path, match)
       raise Bukin::InstallError, "The resource #{data[:name]} (#{data[:version]}) has no jar files in it's download (zip file)." if file_names.empty?
     else
-      save_download(dl_data, dl_name, path)
+      self.save_download(dl_data, dl_name, path)
       file_names << dl_name
     end
 
@@ -49,7 +49,27 @@ class Bukin::Installer
     file_names
   end
 
-  def self.get_match(match)
+  def save_download(data, name, path)
+    FileUtils.mkdir_p(path)
+    open("#{path}/#{name}", "wb") do |file|
+      file.print data
+    end
+  end
+
+  def download_file(url, content_disposition = false)
+    open(url, "User-Agent" => "Bukin #{Bukin::VERSION}") do |download|
+      file_name = if download.meta['content-disposition']
+                    download.meta['content-disposition'].match(/filename=(\"?)(.+)\1/)[2]
+                  else
+                    File.basename(url)
+                  end
+      file_name = file_name.force_encoding('UTF-8') if file_name.respond_to?(:force_encoding)
+      data = download.read
+      return data, file_name
+    end
+  end
+
+  def get_match(match)
     case match
     when Regexp
       match
@@ -63,4 +83,5 @@ class Bukin::Installer
       raise Bukin::InstallError, "The extract option #{match} is not valid.  Please use a String, Regexp or :all"
     end
   end
+
 end
--- spec/installer_spec.rb
+++ spec/installer_spec.rb
@@ -5,33 +5,37 @@ require 'bukin/exceptions'
 
 describe Bukin::Installer do
   describe :get_match do
+    before :all do
+      @installer = Bukin::Installer.new(nil)
+    end
+
     it 'directly returns a regex' do
-      Bukin::Installer::get_match(/something/).should == /something/
+      @installer.get_match(/something/).should == /something/
     end
 
     it 'wraps a string with start and end anchors' do
-      Bukin::Installer::get_match('something').should == /^something$/
+      @installer.get_match('something').should == /^something$/
     end
 
     it 'matches anything for :all' do
-      Bukin::Installer::get_match(:all).should == //
+      @installer.get_match(:all).should == //
     end
 
     it 'matches .jar files by default' do
-      Bukin::Installer::get_match(nil).should == /\.jar$/
+      @installer.get_match(nil).should == /\.jar$/
     end
 
     it 'errors when given anything else' do
       expect do
-        Bukin::Installer::get_match({})
+        @installer.get_match({})
       end.to raise_error(Bukin::InstallError)
 
       expect do
-        Bukin::Installer::get_match([])
+        @installer.get_match([])
       end.to raise_error(Bukin::InstallError)
 
       expect do
-        Bukin::Installer::get_match(Object.new)
+        @installer.get_match(Object.new)
       end.to raise_error(Bukin::InstallError)
     end
   end
